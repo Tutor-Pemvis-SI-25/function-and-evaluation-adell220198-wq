8b153b716278493e07b67efbccdbf125606b53fe
adell220198-wq <adell220198@gmail.com>
2025-11-18 21:18:31 +0700

    okee

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,14 @@
-//Nama - NIM
+//Adellita Varesu Simamora - 12S25025
 
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    public static void main(String[] args) {
+        int totalData;
+        String command;
+        int[] deskripsi = new int[10], kode = new int[10], matkul = new int[10], dosen = new int[10], deadline = new int[10], ketID = new int[10], kesulitan = new int[10], hari = new int[10], status = new int[10], prioritas = new int[10];
+
+        totalData = 0;
+    }
+}
